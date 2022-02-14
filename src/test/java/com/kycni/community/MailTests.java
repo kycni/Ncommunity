d8fd02aa151bdb120d7ed43785d7689307f3c0cb
kyoni <syq278872032@gmail.com>
2022-02-14 08:44:07 +0800

    完成发邮件功能

---
+++ src/test/java/com/kycni/community/MailTests.java
@@ -0,0 +1,38 @@
+package com.kycni.community;
+
+import com.kycni.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+/**
+ * @author Kycni
+ * @date 2022/2/13 19:24
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = NcommunityApplication.class)
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+    @Autowired
+    private TemplateEngine templateEngine;
+    
+    @Test
+    public void testSendEmail () {
+        mailClient.sendMail("2242602678@qq.com", "宝宝", "发邮件功能搞定！！");
+    }
+    
+    @Test
+    public void testSendHtml () {
+        Context context = new Context();
+        context.setVariable("username", "kycni");
+        String content = templateEngine.process("/mail/demo", context);
+        mailClient.sendMail("coder417@sina.com", "HTML", "发邮件功能搞定！！");
+    }
+}
